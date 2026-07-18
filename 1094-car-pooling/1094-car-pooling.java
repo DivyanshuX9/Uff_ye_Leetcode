e1a776378bc7011fa8954181ad7237cff34f4923
Divyanshu Sharma <155464075+DivyanshuX9@users.noreply.github.com>
2026-07-18 14:18:54 +0530

    Time: 617 ms (5.06%), Space: 47.4 MB (6.04%) - LeetHub

---
+++ 1094-car-pooling/1094-car-pooling.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        Arrays.sort(trips,(a,b)->Integer.compare(a[1], b[1]));
+        System.out.println(trips[trips.length-1][2]);
+        int[] timeline=new int[trips[trips.length-1][2]+10000];
+
+        for(int a[]:trips){
+            timeline[a[1]]+=a[0];
+            timeline[a[2]]-=a[0];
+            if(timeline[a[1]]>capacity) return false;
+            int x=a[1]+1;
+            while(x<=a[2]) timeline[x++]+=a[0];
+        }
+        for(int a:timeline) System.out.println(a);
+        return true;
+    }
+}
