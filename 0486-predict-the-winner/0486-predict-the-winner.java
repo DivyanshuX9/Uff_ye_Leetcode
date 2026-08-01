eeb6370b28e2b58a10ba7ba47d7658dee87eb9d1
Divyanshu Sharma <155464075+DivyanshuX9@users.noreply.github.com>
2026-08-01 21:42:17 +0530

    Time: 1 ms (51.87%), Space: 42.6 MB (74.96%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean iter(int[] nums, int l, int r, int p1, int p2, boolean turn) {
+        if (l > r) {
+            return p1 >= p2;
+        }
+
+        if (turn) {
+            return iter(nums, l + 1, r, p1 + nums[l], p2, false) || iter(nums, l, r - 1, p1 + nums[r], p2, false);
+        } else {
+            return iter(nums, l + 1, r, p1, p2 + nums[l], true) && iter(nums, l, r - 1, p1, p2 + nums[r], true);
+        }
+    }
+
+    public boolean predictTheWinner(int[] nums) {
+        return iter(nums, 0, nums.length - 1, 0, 0, true);
+    }
+}
