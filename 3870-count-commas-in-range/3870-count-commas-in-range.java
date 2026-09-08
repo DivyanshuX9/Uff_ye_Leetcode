a12ccd34007b62d0da00b4a75d92722286fb2eee
Divyanshu Sharma <155464075+DivyanshuX9@users.noreply.github.com>
2026-09-08 12:53:04 +0530

    Time: 1 ms (83.65%), Space: 42.5 MB (57.08%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,13 +1,6 @@
 class Solution {
     public int countCommas(int n) {
         if(n<1000) return 0;
-
-        // String s=n.toString();
-
-        // int l=s.length();
-
-        // l/=3;
-        
         return (n-1000)+1;
     }
 }
